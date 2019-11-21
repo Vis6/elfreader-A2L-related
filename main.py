332c2227ca129cde1da2c4cd648011d4e60647ff
Ximu Zhang <ximuzh@gmail.com>
2019-11-21 16:43:06 -0500

    add a2l_generator

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Date: 11/17/2019
 # import libraries
 from __future__ import print_function
 from elf_reader import *
+from a2l_generator import *
 
 
 def process_file(filename):
@@ -17,6 +18,7 @@ def process_file(filename):
 	elf_file, elf_header_info = get_elf_header(filename)
 	elf_section_info = get_section_info(elf_file)  # retrieve the section information
 	global_symbol_set = get_global_symbols(elf_file)  # retrieve section info
+	a2l_variable_section_write(global_symbol_set)
 
 
 if __name__ == '__main__':

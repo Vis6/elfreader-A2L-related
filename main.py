f83480925547fcd78bd451ff16bfcd66eac1e57c
Ximu Zhang <ximuzh@gmail.com>
2019-12-02 15:53:39 -0400

    add a2l_merge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Program to read .elf file.
+Program to read .elf file, auto create A2L and merge A2L files.
 Author: Ximu Zhang
 Date: 11/17/2019
 """
@@ -8,20 +8,28 @@ Date: 11/17/2019
 from __future__ import print_function
 from elf_reader import *
 from a2l_generator import *
+from a2l_merge import *
 
 
 def process_file(filename):
 	"""
 	:param filename: the path of the .elf file
-	:return: None
+	:return: elf_header_info, elf_section_info, global_symbol_set
 	"""
 	elf_file, elf_header_info = get_elf_header(filename)
 	elf_section_info = get_section_info(elf_file)  # retrieve the section information
-	get_debug_info(elf_file)
+	# get_debug_info(elf_file)  # TODO: need to be finished
 	global_symbol_set = get_global_symbols(elf_file)  # retrieve section info
 	a2l_variable_section_write(global_symbol_set)
+	return elf_header_info, elf_section_info, global_symbol_set
 
 
 if __name__ == '__main__':
+	# read elf file
 	file_path = '.\\input\\sample.elf'
-	process_file(file_path)
+	elf_header_info, elf_section_info, global_symbol_set = process_file(file_path)
+
+	# merge a2l files
+	a2l_file_path = '.\\input\\xx.a2l'
+	a2l_file_path2 = '.\\input\\xx.a2l'
+	combined_a2l_file = merge_a2l(a2l_file, a2lfile2)

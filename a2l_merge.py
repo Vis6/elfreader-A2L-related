81137bd3a92c295afcf2ae87db3e37b9b66cae4a
Ximu Zhang <ximuzh@gmail.com>
2019-12-02 15:55:27 -0400

    file containing functions to merge several a2l files

---
+++ a2l_merge.py
@@ -0,0 +1,15 @@
+"""
+Program to merge A2L file and give address to each variable.
+Author: Ximu Zhang
+Date: 12/02/2019
+"""
+
+def merge_a2l(a2l_file, combined_a2l_file):
+	"""
+	Function to merge a2l file.
+	:param a2l_file: a2l file to be merged
+	:param combined_a2l_file: combination of several a2l files
+	:return: combined a2l file
+	"""
+	new_combined_a2l_file = combined_a2l_file
+	return new_combined_a2l_file

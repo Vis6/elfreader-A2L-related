141f3df18776f67387f68c6a8bba5206dcff9ae1
Ximu Zhang <ximuzh@gmail.com>
2019-12-02 20:38:36 -0400

    change the function parameter names

--- a2l_merge.py
+++ a2l_merge.py
@@ -4,12 +4,13 @@ Author: Ximu Zhang
 Date: 12/02/2019
 """
 
-def merge_a2l(a2l_file, combined_a2l_file):
+
+def merge_a2l(a2l_file_path, combined_a2l_file_path):
 	"""
 	Function to merge a2l file.
-	:param a2l_file: a2l file to be merged
-	:param combined_a2l_file: combination of several a2l files
+	:param a2l_file_path: a2l file to be merged
+	:param combined_a2l_file_path: combination of several a2l files
 	:return: combined a2l file
 	"""
-	new_combined_a2l_file = combined_a2l_file
+	new_combined_a2l_file = combined_a2l_file_path
 	return new_combined_a2l_file

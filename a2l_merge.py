66a4500afb981b27c9ffd61e9ccd839d6f7c22c6
Ximu Zhang <ximuzh@gmail.com>
2019-12-05 18:10:11 -0500

    add more parts to form the A2L

--- a2l_merge.py
+++ a2l_merge.py
@@ -5,11 +5,15 @@ Date: 12/02/2019
 """
 
 
-def merge_a2l(measurement, parameter, a2l_file_path):
+def merge_a2l(measurement, parameter, compu_method, compu_tab, function, record_layout, a2l_file_path):
 	"""
 	Function to merge a2l file. (currently only merge the characteristic and measurement parts)
 	:param measurement: combination of measurement
 	:param parameter: combination of parameter
+	:param compu_method: combination of parameter
+	:param compu_tab: combination of parameter
+	:param function: combination of parameter
+	:param record_layout: combination of parameter
 	:param a2l_file_path: single a2l file path
 	:return: combined measurement variables and combined parameter variables
 	"""
@@ -17,45 +21,118 @@ def merge_a2l(measurement, parameter, a2l_file_path):
 		text = ''  # initial text
 		measurement_flag = False
 		parameter_flag = False
+		compu_method_flag = False
+		compu_tab_flag = False
+		function_flag = False
+		record_layout_flag = False
 		line = f.readline()
 		while line:
 			if '/begin CHARACTERISTIC' in line:
 				text += line
 				parameter_flag = True
-			elif '/begin MEASUREMENT' in line:
-				text += line
-				measurement_flag = True
 			elif '/end CHARACTERISTIC' in line:
 				text += line
 				parameter_flag = False
 				parameter.append(text)
 				text = ''  # clear
+
+			elif '/begin MEASUREMENT' in line:
+				text += line
+				measurement_flag = True
 			elif '/end MEASUREMENT' in line:
 				text += line
 				measurement_flag = False
 				measurement.append(text)
 				text = ''  # clear
+
+			elif '/begin COMPU_METHOD' in line:
+				text += line
+				compu_method_flag = True
+			elif '/end COMPU_METHOD' in line:
+				text += line
+				compu_method_flag = False
+				compu_method.append(text)
+				text = ''  # clear
+
+			elif '/begin COMPU_VTAB' in line:
+				text += line
+				compu_tab_flag = True
+			elif '/end COMPU_VTAB' in line:
+				text += line
+				compu_tab_flag = False
+				compu_tab.append(text)
+				text = ''  # clear
+
+			elif '/begin FUNCTION' in line:
+				text += line
+				function_flag = True
+			elif '/end FUNCTION' in line:
+				text += line
+				function_flag = False
+				function.append(text)
+				text = ''  # clear
+
+			elif '/begin RECORD_LAYOUT' in line:
+				text += line
+				record_layout_flag = True
+			elif '/end RECORD_LAYOUT' in line:
+				text += line
+				record_layout_flag = False
+				record_layout.append(text)
+				text = ''  # clear
+
 			else:
-				if measurement_flag or parameter_flag:
+				if measurement_flag or parameter_flag or compu_method_flag or compu_tab_flag or function_flag or record_layout_flag:
 					text += line
 				else:
 					pass
 			line = f.readline()
 	f.close()
-	return measurement, parameter
+	return measurement, parameter, list(set(compu_method)), list(set(compu_tab)), list(set(function)), list(
+		set(record_layout))
 
 
-def form_combined_a2l(variable_list):
+def form_combined_a2l(variable_list, compu_method_list, compu_tab_list, function_list, record_layout_list):
 	"""
 	Function to write a a2l file.
 	:param variable_list: combined variables including measurements and parameters from all the rings
+	:param compu_method_list: combined computation methods from all the rings
+	:param compu_tab_list: combined computation tables from all the rings
+	:param function_list: combined functions from all the rings
+	:param record_layout_list: combined record layouts from all the rings
 	:return: None
 	"""
+
 	f = open('.\\out\\combined.a2l', 'w')
-	for variable in variable_list:
-		f.write(variable)
-		f.write('\n')  # add blank line between each variable
+
+	# write template - project, module - begin / A2ML - begin and end
+	f.write('/begin PROJECT NONAME ""\n  /begin MODULE NONAME ""\n    /begin A2ML\n    /end A2ML\n')
+
+	# component parts
+	# for variable in variable_list:
+	# 	f.write(variable)
+	# 	f.write('\n')  # add blank line between each variable
+	write_component(variable_list, f)
+	write_component(compu_method_list, f)
+	write_component(compu_tab_list, f)
+	write_component(function_list, f)
+	write_component(record_layout_list, f)
+
+	# write template - project, module - end
+	f.write('/end PROJECT\n  /end MODULE\n')
 	f.close()
+
+
+def write_component(parameter, output_file):
+	"""
+	Function to write the a2l components to the output file.
+	:param parameter: a2l component
+	:param output_file: output file
+	:return: None
+	"""
+	for param in parameter:
+		output_file.write(param)
+		output_file.write('\n')  # add blank line between each variable
 
 
 def modify_address(variable_list, symbol_set):
